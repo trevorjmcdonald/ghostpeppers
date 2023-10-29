a4c115c5e9cb59e44a8ad16877b71f7ec9b193a1
Trevor McDonald <trevor.j.mcdonald.personal@gmail.com>
2023-10-29 01:32:09 -0400

    Removed submodules

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ workspace "GhostPeppers"
     architecture "x86_64"
     configurations { "Debug", "Release" }
 
-project "game"
+project "GhostPeppers"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
@@ -12,44 +12,9 @@ project "game"
     includedirs {
         "external/SDL/include",
         "external/imgui",
+        "external/imgui/backends",
         "external/flecs/include"
     }
-
-    -- SDL2 configuration
-    project "SDL"
-        kind "StaticLib"
-        language "C"
-        location "external/SDL"
-        
-        -- Add SDL source files here
-        files {
-            "external/SDL/src/*.c",
-            -- Add more SDL source files if needed
-        }
-
-    -- ImGui configuration
-    project "ImGui"
-        kind "StaticLib"
-        language "C++"
-        location "external/imgui"
-        
-        -- Add ImGui source files here
-        files {
-            "external/imgui/*.cpp",
-            -- Add more ImGui source files if needed
-        }
-
-    -- Flecs configuration
-    project "Flecs"
-        kind "StaticLib"
-        language "C"
-        location "external/flecs"
-        
-        -- Add Flecs source files here
-        files {
-            "external/flecs/src/*.c",
-            -- Add more Flecs source files if needed
-        }
 
     filter "configurations:Debug"
         symbols "On"
